bdc885f6a6b1bad270ef1885ae7da688ef37988b
seonyeob94 <iseonyeob41@gmail.com>
2025-03-25 19:24:50 +0900

    정리: .sql 파일은 BagicOracle로 이동 완료됨에 따라 제거

--- memo_sql/HR~vo코드조각.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    'private ' || 
-    DECODE(DATA_TYPE, 'NUMBER', 'int ', 'String ') || 
-    LOWER(SUBSTR(REPLACE(INITCAP(REPLACE(LOWER(COLUMN_NAME), '_', ' ')), ' ', ''), 1, 1)) || 
-    SUBSTR(REPLACE(INITCAP(REPLACE(LOWER(COLUMN_NAME), '_', ' ')), ' ', ''), 2) || 
-    ';'
-FROM COLS
-WHERE TABLE_NAME = 'COUNTRIES'
-ORDER BY COLUMN_ID;
